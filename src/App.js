78f5756e6b008b774ca3a445c5d61fb6712f62dd
chibuzor <incovenant@hotmail.com>
2025-05-01 15:04:31 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import AddTask from "./components/AddTask";
+import ListTask from "./components/ListTask";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	return (
+		<div className="app">
+			<h1>ToDo App with Redux</h1>
+			{/* Component for adding new tasks */}
+			<AddTask />
+			{/* Component for displaying and filtering tasks */}
+			<ListTask />
+		</div>
+	);
+};
 
 export default App;
